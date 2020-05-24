1aedf8ebc5f929636066a93ec6e3ab4fd931c670
Airo <pablo_penna@hotmail.com>
2020-05-24 21:27:17 +0200

    Added player projectiles

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public LayerMask damageLayer;
+    int maxHits = 1;
+    Vector2 direction = Vector2.up;
+    public float projectileSpeed = 10f;
+    [SerializeField]
+    float hitboxRadius = 0.5f;
+    public GameObject explosionPrefab;
+    ProjectileShooter launcher; //launcher which created this projectile
+
+    void SetDirection(Vector2 direction) {
+        this.direction = direction;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * projectileSpeed * Time.deltaTime);
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, hitboxRadius, damageLayer);
+        if (hit) {
+            Detonate(hit);
+        }
+
+        RemoveIfOutOfScreen();
+    }
+
+    void Detonate(Collider2D hit) {
+        Destroy(hit.gameObject);
+        launcher.RemoveProjectile(gameObject);
+        Destroy(gameObject);
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+    }
+
+    public void SetLauncher(ProjectileShooter launcher) {
+        this.launcher = launcher;
+    }
+
+    void RemoveIfOutOfScreen() { 
+        if(transform.position.y > 5) {
+            launcher.RemoveProjectile(gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectileShooter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileShooter : MonoBehaviour
+{
+    [SerializeField]
+    GameObject projectilePrefab;
+    [SerializeField]
+    GameObject shootingPoint;
+    List<GameObject> launchedProjectiles;
+    public int maxLaunchedProjectiles = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        launchedProjectiles = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && launchedProjectiles.Count < maxLaunchedProjectiles) {
+            GameObject projectile = Instantiate(projectilePrefab, shootingPoint.transform.position, Quaternion.identity);
+            Projectile projectileScript = projectile.GetComponent<Projectile>();
+            projectileScript.SetLauncher(this);
+            launchedProjectiles.Add(projectile);
+        }
+    }
+
+    public void RemoveProjectile(GameObject projectile) {
+        this.launchedProjectiles.Remove(projectile);
+    }
+}
