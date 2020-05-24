5a30eba1790ac155417e582491835b2b8c14f952
Airo <pablo_penna@hotmail.com>
2020-05-24 17:44:30 +0200

    Added sprites and basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float xMovement = xInput * this.moveSpeed * Time.deltaTime;
+        transform.Translate(new Vector2(xMovement, 0f));
+    }
+}
